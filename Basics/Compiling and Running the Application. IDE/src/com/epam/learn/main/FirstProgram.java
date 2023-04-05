b07bb5c7004ca532bb099b3536c896e41c6f256c
piter11251 <duszeq13@gmail.com>
2023-04-06 00:07:04 +0200

    first programs

---
+++ Basics/Compiling and Running the Application. IDE/src/com/epam/learn/main/FirstProgram.java
@@ -0,0 +1,15 @@
+package com.epam.learn.main;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 16);
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
+}
---
+++ Basics/Compiling and Running the Application. IDE/src/com/epam/learn/main/InputStringProgram.java
@@ -0,0 +1,11 @@
+package com.epam.learn.main;
+
+import java.util.Scanner;
+
+public class InputStringProgram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        System.out.println("Hello "+input);
+    }
+}
